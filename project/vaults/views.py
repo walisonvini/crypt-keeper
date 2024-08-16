5d6cc4d7bb700cf0ad24184ebef1ae31d00478e8
Walison Vinicios <walison.vinicios12@gmail.com>
2024-08-16 17:27:43 -0300

    Update secondary color in global CSS

--- project/vaults/views.py
+++ project/vaults/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 @login_required
-def index_view(request):
-    return render(request, 'index.html') 
+def vault_view(request):
+    return render(request, 'vault.html')
