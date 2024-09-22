72d2a9e2cf20d747b79123daf1a1169aa78da791
walisonvini <walison.vinicios12@gmail.com>
2024-09-22 19:50:22 -0300

    Update ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS in settings.py

--- project/setup/settings.py
+++ project/setup/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = [
     'crypt-keeper-production.up.railway.app'
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://crypt-keeper-production.up.railway.app',
+]
+
 
 # Application definition
 
@@ -130,6 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
