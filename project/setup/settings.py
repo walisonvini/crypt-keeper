679d4b9f9202ef60617bfd2e7b05954d13dac285
walisonvini <walison.vinicios12@gmail.com>
2024-09-22 19:33:08 -0300

    Update database configuration in settings.py and railway.json

--- project/setup/settings.py
+++ project/setup/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'IjHIiubgJMkArQlhWVqNIwOWYxZrTyIl',
+        'HOST': 'autorack.proxy.rlwy.net',
+        'PORT': '56397'
     }
 }
 
