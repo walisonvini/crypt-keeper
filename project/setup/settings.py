6129d6abffa5b8123f9e04d449fd247a407e70ad
walisonvini <walison.vinicios12@gmail.com>
2024-09-22 20:13:59 -0300

    Update STATICFILES_DIRS and STATIC_ROOT paths in settings.py

--- project/setup/settings.py
+++ project/setup/settings.py
@@ -134,7 +134,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'project/static')]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
