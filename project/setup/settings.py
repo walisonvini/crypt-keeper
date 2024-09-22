c35defcf5a55f6b78b91c0e1f4eeaa92001fae5f
walisonvini <walison.vinicios12@gmail.com>
2024-09-22 20:24:54 -0300

    Update STATICFILES_DIRS and STATIC_ROOT paths in settings.py

--- project/setup/settings.py
+++ project/setup/settings.py
@@ -28,6 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
+    '127.0.0.1',
     'crypt-keeper-production.up.railway.app'
 ]
 
@@ -133,7 +134,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
