d6af2277d20a7da7c65749864465f9df2c0a7ff3
walisonvini <walison.vinicios12@gmail.com>
2025-08-20 20:55:24 -0300

    Update environment configuration to include ALLOWED_HOSTS, modify Dockerfile to use pip directly, and adjust settings.py to parse ALLOWED_HOSTS from environment variables.

--- setup/settings.py
+++ setup/settings.py
@@ -27,13 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG")
+DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    'cryptkeeper.walisonribeiro.com'
-]
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 CSRF_TRUSTED_ORIGINS = [
     'https://cryptkeeper.walisonribeiro.com',
